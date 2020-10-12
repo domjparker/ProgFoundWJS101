ac0b6690ced61c673bdb285c14e921959c86e73e
Rees Dart <thedominicparker@gmail.com>
2020-10-12 13:15:36 -0700

    toying with basic calculation code

---
+++ lesson2/pseudocode.js
@@ -0,0 +1,32 @@
+// TODO: a function that returns the sum of two numbers
+// START 
+// # Given 2 integers
+// SET sum = 1st integer + 2nd integer
+// RETURN sum
+// END
+
+// TODO: a function that takes an array of strings, and returns a string that is all those strings concatenated together
+// START
+// # Given a collection of strings called "array"
+// SET iterator = 1
+// SET savedString = array[0]
+// WHILE iterator <= length of array
+  // SET currentString = savedString + value of the string collection at 'iterator'
+  // iterator = iterator + 1
+// RETURN savedString
+// END
+
+// TODO: a function that takes an array of integers, and returns a new array with every other element
+// START
+// # Given a collection of integers called "array"
+// SET iterator = 1
+// SET newArray
+// WHILE iterator < length of array
+  // SET currentInteger = value of array at integer
+  // IF iterator === 1
+  // add to new array
+  // ELSE IF iterator === divided by 2, has a remainder of 1
+  // add to new array
+  // ELSE skip to the next iteration
+  // iterator = iterator + 1
+//END
