78bde56a4592f47e8e5bc0ba2658ee70bb602691
Rees Dart <thedominicparker@gmail.com>
2020-10-13 08:19:55 -0700

    adding to calculator

--- lesson2/calculator.js
+++ lesson2/calculator.js
@@ -5,6 +5,11 @@
 // Print the result to the terminal.
 
 const readline = require('readline-sync');
+const MESSAGES = require('./calculator_messages.json')
+
+function messages(message, lang= 'es') {
+  return MESSAGES[lang][message]
+}
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -14,57 +19,76 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-prompt('Welcome to Calculator');
+function calculate() {
+  prompt(messages('firstNum', 'es'));
+  let number1 = readline.question();
+  // prompt(number1);
+  while (invalidNumber(number1)) {
+    prompt(messages.invalidNum);
+    number1 = readline.question();
+  }
 
-prompt("What's the first number?");
-let number1 = readline.question();
-// prompt(number1);
-while (invalidNumber(number1)) {
-  prompt("hmm... that doesn't look like a valid number.");
-  number1 = readline.question();
+  prompt(messages('secondNum', 'es'));
+  let number2 = readline.question();
+  // prompt(`${number1} ${number2}`);
+  while (invalidNumber(number2)) {
+    prompt(messages.invalidNum);
+    number2 = readline.question();
+  }
+
+  prompt(messages('whatOp', 'es'));
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(messages('mustChoose', 'es'));
+    operation = readline.question();
+  }
+
+  let output;
+  // if (operation === '1') {
+  //   output = Number(number1) + Number(number2);
+  // } else if ( operation === '2') {
+  //   output = Number(number1) - Number(number2);
+  // } else if ( operation === '3') {
+  //   output = Number(number1) * Number(number2);
+  // } else if ( operation === '4') {
+  //   output = Number(number1) / Number(number2);
+  // }
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(`The result is: ${output}`);
+  repeatCalc();
 }
 
-prompt("What's the second number?");
-let number2 = readline.question();
-// prompt(`${number1} ${number2}`);
-while (invalidNumber(number2)) {
-  prompt("hmm... that doesn't look like a valid number.");
-  number2 = readline.question();
+function repeatCalc() {
+  prompt(messages('anotherCalc', 'es'));
+  let repeatAnswer = readline.question();
+  if (repeatAnswer[0].toLocaleLowerCase() === "y" || "s") {
+    calculate();
+  } else {
+    console.log(messages('exitThanks', 'es'));
+  }
 }
 
-prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
-let operation = readline.question();
+prompt(messages('welcome', 'en'));
+calculate();
 
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Must choose 1, 2, 3, or 4');
-  operation = readline.question();
-}
 
-let output;
-// if (operation === '1') {
-//   output = Number(number1) + Number(number2);
-// } else if ( operation === '2') {
-//   output = Number(number1) - Number(number2);
-// } else if ( operation === '3') {
-//   output = Number(number1) * Number(number2);
-// } else if ( operation === '4') {
-//   output = Number(number1) / Number(number2);
-// }
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
-}
 
-prompt(`The result is: ${output}`);
+
+
 
 
