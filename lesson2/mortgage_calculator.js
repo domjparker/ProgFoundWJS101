c48fedbffa1cc14e3acd09926876bd7ac26c9a90
Rees Dart <thedominicparker@gmail.com>
2020-10-20 21:42:43 -0700

    refactored mortgage_calculator.jsand fixed issues.

--- lesson2/mortgage_calculator.js
+++ lesson2/mortgage_calculator.js
@@ -10,47 +10,63 @@
 // Ask user if they would like to do another calculation
 
 // require readline for getting answers to question prompts
-const READLINE = require('readline-sync')
+const READLINE = require('readline-sync');
+
+// function to clear screen
+function clear4Lines() {
+  console.log('\n\n\n\n');
+}
 
 // function to tidy message display
 function prompt(message) {
-  console.log(`=> ${message}`);
+  console.log(`\n=> ${message}`);
 }
 
 // function to validate loan amount input
-function validLoanAmount(inputValue) {
-  if (inputValue.includes(' thousand' || ' grand' || ' thousand dollars')) {
-    let refinedValue = parseInt(inputValue.replace(' thousand' || ' grand' || 'thousand dollars', '')) * 1000;
+function getValidLoanAmount() {
+  // Ask user for the amount they want to borrow for mortgage
+  prompt(`What is the amount you would like to borrow in this mortgage loan?`);
+  let loanInputValue = READLINE.question();
+  if ((loanInputValue.includes('-')) || (Math.sign(loanInputValue) === -1) || (Number(loanInputValue) === 0)
+  || (isNaN(loanInputValue))) {
+    prompt('You have entered an invalid loan amount.');
+    runMainProgram();
+  }
+  if (loanInputValue.includes(' thousand' || ' grand' || ' thousand dollars')) {
+    let refinedValue = Number(loanInputValue.replace(' thousand' || ' grand' || 'thousand dollars', '')) * 1000;
     return refinedValue;
   } else {
-    refinedValue = parseInt(inputValue.replace(/[^\d.-]/g, ''));
+    let refinedValue = Number(loanInputValue.replace(/[^\d.-]/g, ''));
     return refinedValue;
   }
 }
 
 // function to validate APR input
-function validPercentage(value) {
-  if (!value.includes('%')) {
-    prompt('Please enter a valid percentage value.')
-    getMortRate();
+function getValidAPR() {
+  // Ask user for annual percentage rate (APR) they have been quoted
+  prompt(`What is the annual percentage rate (APR) you have been quoted? (e.g. 3.5%, 4%, 4.5%)`);
+  let annualPercentageRateInputValue = READLINE.question();
+  if (!annualPercentageRateInputValue.includes('%')) {
+    prompt('You have entered an invalid percentage Annual Percentage Rate (APR).');
+    runMainProgram();
   }
-  if (value.includes('%')) {
-    return parseFloat(value.replace('%', '')) / 100;
-  }
+  return parseFloat((annualPercentageRateInputValue.replace('%', '')) / 100);
 }
 
-// function to validate loan year duration (somewhat redundant, but I'm just experimenting)
-function validateDuration(durationAnswer) {
-  if (durationAnswer === 'other') {
-    prompt('Over how many years will the loan be repaid? (e.g 5, 7, 13, 24)');
-    durationAnswer2 = READLINE.question();
-    yearAnswerValid = validateDuration(durationAnswer2);
-    return yearAnswerValid;
-  } else if (durationAnswer.includes(' years')) {
-    return durationAnswer.replace(' years', '');
-  } else {
-    return durationAnswer;
+// function to validate loan year duration (somewhat redundant, but I'm
+// just experimenting)
+function getValidDuration() {
+  // Ask user for the duration of loan they have been chosen (15, 30, or
+  // other years)
+  prompt('Will this be repaid over 15 years, 30 years, or other? (e.g 15, 30, 7, 13, 24)');
+  let durationAnswer = READLINE.question();
+  if (durationAnswer.includes(' years')) {
+    durationAnswer = Number(durationAnswer.replace(' years', ''));
+  } else if (isNaN(durationAnswer)) {
+    prompt('Please enter a valid number amount of years for the loan duration');
+    runMainProgram();
   }
+  return durationAnswer;
 }
 
 // function to insert commas as thousands separators
@@ -59,55 +75,65 @@ function addCommas(num) {
 }
 
 // Create function to allow for repeated calculations
-function getMortRate() {
-  // Ask user for the amount they want to borrow for mortgage
-  prompt(`What is the amount you would like to borrow in this mortgage loan?`);
-  let initLoanAmount = READLINE.question();
-  let loanAmount = validLoanAmount(initLoanAmount);
+function getUserInputsForCalculation() {
+  let loanAmount = getValidLoanAmount();
   // console.log(loanAmount);
   // console.log(typeof loanAmount);
-  // Ask user for annual percentage rate (APR) they have been quoted
-  prompt(`What is the annual percentage rate (APR) you have been quoted? (e.g. 3.5%, 4%, 4.5%)`);
-  let initAnnPercRate = READLINE.question();
-  let annPercRate = validPercentage(initAnnPercRate);
+  let annPercRate = getValidAPR();
   // console.log(annPercRate);
   // console.log(typeof annPercRate);
-  // Ask user for the duration of loan they have been chosen (15, 30, or other years)
-  prompt('Will this be repaid over 15 years, 30 years, or other? (15/30/other)');
-  let yearAnswer = READLINE.question();
-  let loanYearDur = validateDuration(yearAnswer);
-  calcMonthPayment(loanAmount, annPercRate, loanYearDur);
+  let loanYearDur = getValidDuration();
+  return {
+    loanAmount,
+    annPercRate,
+    loanYearDur
+  };
 }
 
-// function to calculate monthly repayment
-function calcMonthPayment(loanAm, aPR, loanYears) {
+// function to calculate and display monthly repayment
+function displayMonthlyPayment(validUserInputs) {
+  let vUI = validUserInputs;
   // convert APR to monthly interest rate
-  let monthPercRate = aPR / 12;
+  let monthPercRate = vUI.annPercRate / 12;
   // convert loan years amount to months
-  let monthLoanDur = loanYears * 12;
+  let monthLoanDur = vUI.loanYearDur * 12;
   // run formula
-  let monthPayment = addCommas((loanAm * (monthPercRate / (1 - Math.pow((1 + monthPercRate), (-monthLoanDur))))).toFixed(2));
-  let totalPayment = (monthPayment * 12 * loanYears).toFixed(2);
-  let repaymentDifference = addCommas((totalPayment - loanAm).toFixed(2));
+  let monthPayment = addCommas((vUI.loanAmount * (monthPercRate /
+    (1 - Math.pow((1 + monthPercRate),
+      (-monthLoanDur))))).toFixed(2));
+  let totalPayment = (monthPayment * 12 * vUI.loanYearDur).toFixed(2);
+  let repaymentDifference = addCommas((totalPayment -
+    vUI.loanAmount).toFixed(2));
   // log results for user
-  prompt(`Your monthly repayment for this mortgage will be $${monthPayment}. Over a period of ${loanYears} years (${monthLoanDur} months), your total repayment amount comes to $${addCommas(totalPayment)}. Therefore, on top of the repayment of your initial loan amount ($${addCommas(loanAm)}) you will end up paying an extra $${repaymentDifference} to the loaning institution. Is it worth it?`);
-  repeatCalc();
+  prompt(`Your monthly repayment for this mortgage will be $${monthPayment}. 
+  Over a period of ${vUI.loanYearDur} years (${monthLoanDur} months), your total 
+  repayment amount comes to $${addCommas(totalPayment)}. Therefore, on top of 
+  the repayment of your initial loan amount ($${addCommas(vUI.loanAmount)}) you 
+  will end up paying an extra $${repaymentDifference} to the loaning institution. 
+  Is it worth it?`);
 }
 
 // function to repeat calculation
-function repeatCalc() {
+function repeatCalculationYesNo() {
   prompt('Would you like to do another calculation? (y/n)');
   let answerRepeat = READLINE.question();
   if (answerRepeat[0].toLocaleLowerCase() === 'y') {
-    getMortRate();
-  } else {
-    prompt('Thanks for using this mortgage calculator.');
+    runMainProgram();
   }
 }
 
-// Welcome
-prompt('Welcome. This is a mortgage calculator. Answer these questions to calculate the amount you would be forecasted to repay.');
-// Run initial program
-getMortRate();
+function runMainProgram() {
+  // get necessary inputs from user
+  let validUserInputs = getUserInputsForCalculation();
+  displayMonthlyPayment(validUserInputs);
+  repeatCalculationYesNo();
+}
 
+// Welcome
+clear4Lines();
+prompt('Welcome. This is a mortgage calculator. Answer these questions to forecast the amount you would have to repay.');
+runMainProgram();
+
+
+prompt('Thanks for using this mortgage calculator.');
 
---
+++ lesson2/rock_paper_scissors.js
@@ -0,0 +1,46 @@
+const readline = require('readline-sync');
+VALID_CHOICES = ['rock', 'paper', 'scissors'];
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function displayWinner(choice, computerChoice) {
+  if ((choice === 'rock' && computerChoice === 'scissors') ||
+    (choice === 'scissors' && computerChoice === 'paper') ||
+    (choice === 'paper' && computerChoice === 'rock')) {
+    prompt('You Win!');
+  } else if ((choice === 'rock' && computerChoice === 'paper') ||
+    (choice === 'scissors' && computerChoice === 'rock') ||
+    (choice === 'paper' && computerChoice === 'scissors')) {
+    prompt('Computer Wins!');
+  } else { 
+    prompt("It's a tie.") 
+  }
+}
+
+while (true) {
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = readline.question();
+
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt(`That's not a valid choice. Please choose again: ${VALID_CHOICES.join(', ')}`);
+    choice = readline.question;
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+
+  displayWinner(choice, computerChoice);
+
+  prompt("Would you like to play again?")
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter either "y" or "n".');
+    answer = readline.question().toLowerCase();
+  }
+
+  if (answer[0].toLowerCase() !== 'y') break;
+}
