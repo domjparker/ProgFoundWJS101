e9883346662b449f2c052beb6a4084aa4464ac39
Rees Dart <thedominicparker@gmail.com>
2020-11-02 08:37:48 -0800

    exclusive_or and odd_lists

--- smallprobs/easy2/exclusive_or.js
+++ smallprobs/easy2/exclusive_or.js
@@ -1,19 +1,69 @@
 /*
+The || operator returns a truthy value if either or both of its operands are 
+truthy, a falsey value if both operands are falsey. The && operator returns a 
+truthy value if both of its operands are truthy, and a falsey value if either 
+operand is falsey. This works great until you need only one of two conditions 
+to be truthy, the so-called exclusive or.
 
+In this exercise, you will write a function named xor that takes two arguments,
+ and returns true if exactly one of its arguments is truthy, false otherwise. 
+ Note that we are looking for a boolean result instead of a truthy/falsey value 
+ as returned by || and &&.
 */
 // TODO Problem
 /*
-INPUT = 
-OUTPUT = 
+INPUT = two values
+OUTPUT = boolean: true if exactly one of its arguments is truthy, and falsey if
+both are falsey or truthy 
 */
 // TODO Examples
 /*
-
+console.log(xor(5, 0) === true);
+console.log(xor(false, true) === true);
+console.log(xor(1, 1) === false);
+console.log(xor(true, true) === false);
 */
 // TODO Data Structures
-
+// primitive (spec numbers,strings,booleans)
 // TODO Algorithm
 /*
-
+function takes in 2 arguments
+helper function to determine whether each argument is truthy
+if true && true, return false
+if false && false, return false
+otherwise, return true
 */
 // TODO Code
+
+
+// function truthyOrFalsy(val) {
+//   if (val) {
+//       return true
+//   } else {
+//       return false;
+//   }
+// }
+
+// function xor(value1, value2) {
+//   value1 = truthyOrFalsy(value1);
+//   value2 = truthyOrFalsy(value2);
+//   if ((value1 === true) && (value2 === true)) {
+//     return 0;
+//   } else if ((value1 === false) && (value2 === false)) {
+//     return 0;
+//   } else return true;
+// }
+
+
+function xor(value1, value2) {
+  if ((value1 && !value2) || (value2 && !value1)) {
+    return true;
+  } 
+  return false;
+}
+
+
+console.log(xor(5, 0) === true);
+console.log(xor(false, true) === true);
+console.log(xor(1, 1) === false);
+console.log(xor(true, true) === false);
--- smallprobs/easy2/odd_lists.js
+++ smallprobs/easy2/odd_lists.js
@@ -1,19 +1,47 @@
 /*
-
+Write a function that returns an Array that contains every other element of an 
+Array that is passed in as an argument. The values in the returned list should 
+be those values that are in the 1st, 3rd, 5th, and so on elements of the 
+argument Array.
 */
 // TODO Problem
 /*
-INPUT = 
-OUTPUT = 
+INPUT = array
+OUTPUT = array that contains every other element, starting with the first.
 */
 // TODO Examples
 /*
-
+console.log(oddities([2, 3, 4, 5, 6])); // logs [2, 4, 6]
+console.log(oddities([1, 2, 3, 4, 5, 6])); // logs [1, 3, 5]
+console.log(oddities(["abc", "def"])); // logs ['abc']
+console.log(oddities([123])); // logs [123]
+console.log(oddities([])); // logs []
 */
 // TODO Data Structures
-
+// iterating over an array to produce a second array
 // TODO Algorithm
 /*
-
+function takes in an array
+iterate over the array
+add to a second array the first element of argument array, and every 2nd 
+after that
 */
 // TODO Code
+
+// function oddities(array) {
+//   let oddArray = []
+//   for (let i = 0; i < array.length; i += 2) {
+//     oddArray.push(array[i]);
+//   }
+//   return oddArray;
+// }
+
+function oddities(array) {
+  return array.filter(element => array.indexOf(element) % 2 === 0);
+}
+
+console.log(oddities([2, 3, 4, 5, 6])); // logs [2, 4, 6]
+console.log(oddities([1, 2, 3, 4, 5, 6])); // logs [1, 3, 5]
+console.log(oddities(["abc", "def"])); // logs ['abc']
+console.log(oddities([123])); // logs [123]
+console.log(oddities([])); // logs []
