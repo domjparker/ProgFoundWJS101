5526d39e677bc05aa31aa1361816a78b90ee0c8c
Rees Dart <thedominicparker@gmail.com>
2020-11-09 16:21:24 -0800

    adding 2 problems and solutions

--- smallprobs/easy3/fibonacci_num_location_by_length.js
+++ smallprobs/easy3/fibonacci_num_location_by_length.js
@@ -1,19 +1,43 @@
 /*
-
+Write a function that calculates and returns the index of the first Fibonacci 
+number that has the number of digits specified by the argument. (The first 
+Fibonacci number has an index of 1.)
 */
 // TODO Problem
 /*
-INPUT =
-OUTPUT = 
+INPUT = integer
+OUTPUT = the index of the first Fibonacci number has the integer amount of digits.
+argument always is an integer great than at least 1. 
 */
 // TODO Examples
 /*
-
+findFibonacciIndexByLength(2);       // 7
+findFibonacciIndexByLength(10);      // 45
+findFibonacciIndexByLength(16);      // 74
 */
 // TODO Data Structures
-// 
+// numbers
 // TODO Algorithm
 /*
-
+function takes in an integer
+fibonacci array = [0, 1]
+set index to 1, and iterate until integer amount.
+  fibonacci Array [i] is equal to fibonacciArray[i] + fibonacciArray[i-1]
+  if fibonacci Array[i].toString.length === integer, return i.
 */
 // TODO Code
+
+function findFibonacciIndexByLength(integer) {
+  let fibonacciArray = [0, 1];
+  let i = 1;
+  while (String(fibonacciArray[i]).length < integer) {
+    fibonacciArray.push(fibonacciArray[i] + fibonacciArray[i - 1]);
+    i += 1;
+  }
+  console.log(i);
+}
+
+
+findFibonacciIndexByLength(2);       // 7
+findFibonacciIndexByLength(10);      // 45
+findFibonacciIndexByLength(16);      // 74
--- smallprobs/easy3/right_triangles.js
+++ smallprobs/easy3/right_triangles.js
@@ -1,19 +1,80 @@
 /*
-
+Write a function that takes a positive integer, n, as an argument, and logs a 
+right triangle whose sides each have n stars. The hypotenuse of the triangle 
+(the diagonal side in the images below) should have one end at the lower-left 
+of the triangle, and the other end at the upper-right.
 */
 // TODO Problem
 /*
-INPUT =
-OUTPUT = 
+INPUT = positive integer
+OUTPUT = console.log right triangle, with each side having positive integer's 
+amount of stars. The right angle of the triangle should be on the right side.
 */
 // TODO Examples
 /*
+triangle(5);
+
+    *
+   **
+  ***
+ ****
+*****
+
+triangle(9);
+
+        *
+       **
+      ***
+     ****
+    *****
+   ******
+  *******
+ ********
+*********
+
+triangle(3);
+
+  *
+ **
+***
 
 */
 // TODO Data Structures
-// 
+// empty array, adding stars with each loop iteration. 
 // TODO Algorithm
 /*
+function takes in positive integer (num).
+create array with (num) spaces filled with one-space strings
+loop with index 0, iterate until index < num
+with each iteration, string[(num - 1) - i] is changed to *
+console.log(string)
 
 */
 // TODO Code
+
+function triangle(num) {
+  let array = Array(num - 1).fill(" ");
+  for (let i = 0; i < num; i += 1) {
+    array[(num - 1) - i] = '*';
+    console.log(array.join(''));
+  }
+}
+
+/*
+or....
+
+function triangle(height) {
+  let spaces = height - 1;
+  let stars = 1;
+  while (height > 0) {
+    console.log(`${" ".repeat(spaces)}${"*".repeat(stars)}`);
+    spaces -= 1;
+    stars += 1;
+    height -= 1;
+  }
+}
+*/
+
+triangle(5);
+triangle(9);
+triangle(3);
