3db953ffc8f59db49d77611cc739640e0ec7174a
Rees Dart <thedominicparker@gmail.com>
2020-11-11 15:37:33 -0800

    adding 2 problems and solutions.

--- smallprobs/easy3/double_doubles.js
+++ smallprobs/easy3/double_doubles.js
@@ -1,19 +1,75 @@
 /*
-
+Write a function that returns the number provided as an argument multiplied by 
+two, unless the argument is a double number; return double numbers as-is.
 */
 // TODO Problem
 /*
-INPUT =
-OUTPUT = 
+INPUT = number
+OUTPUT = number multiplied by 2, except double numbers should be return without
+any modification.
 */
 // TODO Examples
 /*
-
+twice(37);          // 74
+twice(44);          // 44
+twice(334433);      // 668866
+twice(444);         // 888
+twice(107);         // 214
+twice(103103);      // 103103
+twice(3333);        // 3333
+twice(7676);        // 7676
 */
 // TODO Data Structures
-// 
+// number > string split into two substrings and check equality
+// number > string > array > iterate over array to find matches.
 // TODO Algorithm
 /*
+function takes in a number
+turn number to string
+if string length is odd, return number
+else if string.substring(0, (string.length / 2 ) - 1) is not equal to string.substring(string.length / 2)
+return number multiplied by 2.
+else return number
 
+function takes in a number
+turn number to string and then to array
+if array length is odd, return number (unchanged)
+else do iterative loop: starting at index 0 and going to halfway, 
+if array element at index is not equal to array at index ((array.length / 2) + i)
+return number unchanged.
+else return number multiplied by 2
 */
 // TODO Code
+function twice(number) {
+  let string = String(number);
+  if (string.substring(0, (string.length / 2)) === (string.substring(string.length / 2))) {
+    return number;
+  }
+  return (number * 2);
+}
+
+
+// (this one below is less efficient)
+// function twice(number) {
+//   let array = Array.from(String(number));
+//   if (array.length % 2 === 1) {
+//     return (number * 2);
+//   } else {
+//     for (let i = 0; i < (array.length / 2); i += 1) {
+//       if (array[i] !== array[(array.length / 2) + i]) {
+//         return (number * 2);
+//       } else {
+//         return number;
+//       }
+//     }
+//   }
+// }
+
+twice(37);          // 74
+twice(44);          // 44
+twice(334433);      // 668866
+twice(444);         // 888
+twice(107);         // 214
+twice(103103);      // 103103
+twice(3333);        // 3333
+twice(7676);        // 7676
--- smallprobs/easy3/grade_book.js
+++ smallprobs/easy3/grade_book.js
@@ -1,19 +1,72 @@
 /*
+Write a function that determines the mean (average) of the three scores passed 
+to it, and returns the letter associated with that grade.
 
+Numerical score letter grade list:
+
+90 <= score <= 100: 'A'
+80 <= score < 90: 'B'
+70 <= score < 80: 'C'
+60 <= score < 70: 'D'
+0 <= score < 60: 'F'
+Tested values are all between 0 and 100. There is no need to check for negative
+ values or values greater than 100.
 */
 // TODO Problem
 /*
-INPUT =
-OUTPUT = 
+INPUT = three number arguments between 0 and 100.
+OUTPUT = the letter grade corresponding to the mean (average) of the three numbers
 */
 // TODO Examples
 /*
-
+getGrade(95, 90, 93);    // "A"
+getGrade(50, 50, 95);    // "D"
 */
 // TODO Data Structures
-// 
+// use an object to hold grade letter key value pairs. Otherwise, numbers
 // TODO Algorithm
 /*
+create object letter grades.
 
+function takes in 3 numbers.
+divide sum of 3 numbers to get mean
+dividedMean = mean divided 10, rounded up to nearest integer
+if dividedMean is 6 or less, return "F".
+else return value of pair in letter grades object corresponding to divided mean.
 */
 // TODO Code
+
+const LETTER_GRADES = {
+  10: "A",
+  9: "B",
+  8: "C",
+  7: "D",
+}
+
+function getGrade(num1, num2, num3) {
+  let meanAverage = (num1 + num2 + num3) / 3;
+  let simplifiedMean = Math.ceil(meanAverage / 10);
+  if (simplifiedMean <= 6) {
+    return "F"
+  };
+  return LETTER_GRADES[String(simplifiedMean)];
+}
+
+getGrade(95, 90, 93);    // "A"
+getGrade(50, 50, 95);    // "D"
+
+// function getGrade(grade1, grade2, grade3) {
+//   let average = (grade1 + grade2 + grade3) / 3;
+
+//   if (average >= 90 && average <= 100) {
+//     return 'A';
+//   } else if (average >= 80 && average < 90) {
+//     return 'B';
+//   } else if (average >= 70 && average < 80) {
+//     return 'C';
+//   } else if (average >= 60 && average < 70) {
+//     return 'D';
+//   } else {
+//     return 'F';
+//   }
+// }
