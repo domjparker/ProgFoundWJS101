51c13329c9470648a43bf2d4717887d5696acc74
Rees Dart <thedominicparker@gmail.com>
2020-11-12 21:11:06 -0800

    adding one solution.

--- smallprobs/easy4/teddy_age.js
+++ smallprobs/easy4/teddy_age.js
@@ -1,19 +1,39 @@
 /*
-
+Build a program that randomly generates Teddy's age, and logs it to the 
+console. Have the age be a random number between 20 and 120 (inclusive).
 */
 // TODO Problem
 /*
-INPUT = 
-OUTPUT = 
+INPUT = no input
+OUTPUT = random number between 20 - 120 (inclusive).
 */
 // TODO Examples
 /*
-
+Teddy is 69 years old!
 */
 // TODO Data Structures
-// 
+// simple numbers and 
 // TODO Algorithm
 /*
-
+function takes in no argument.
+create random number between 0 and 100.
+add 10 to random number
+put resulting number in string.
+log string.
 */
 // TODO Code
+
+function randomAge() {
+  return Math.round(Math.random() * 100) + 20;
+}
+
+let teddyAge = randomAge();
+console.log(`Teddy is ${TeddyAge} years old!`);
+
+// or
+function randomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let age = randomBetween(20, 120);
+console.log(`Teddy is ${age} years old!`);
