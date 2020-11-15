5f9ecf6a628e8b9b509108fd550e835f22057efe
Rees Dart <thedominicparker@gmail.com>
2020-11-14 17:27:13 -0800

    adding 2 solutions.

--- smallprobs/easy4/palin_part_1.js
+++ smallprobs/easy4/palin_part_1.js
@@ -1,19 +1,41 @@
 /*
-
+Write a function that returns true if the string passed as an argument is a 
+palindrome, or false otherwise. A palindrome reads the same forwards and 
+backwards. For this problem, the case matters and all characters matter.
 */
 // TODO Problem
 /*
-INPUT = 
-OUTPUT = 
+INPUT = string
+OUTPUT = boolean representing whether input string is a palindrome or not.
 */
 // TODO Examples
 /*
-
+isPalindrome('madam');               // true
+isPalindrome('Madam');               // false (case matters)
+isPalindrome("madam i'm adam");      // false (all characters matter)
+isPalindrome('356653');              // true
 */
 // TODO Data Structures
-// 
+// string > split into array, reversed, and then joined to string
 // TODO Algorithm
 /*
-
+function takes in string
+string variable = string
+stringReversed variable = string split into array, reversed, then joined again
+are both variables equal
+return result
 */
 // TODO Code
+
+let isPalindrome = string => {
+  let stringReversed = string.split('').reverse().join('');
+  return string === stringReversed;
+}
+
+// or
+// let isPalindrome = string => string === string.split('').reverse().join(''); 
+
+console.log(isPalindrome('madam'));               // true
+console.log(isPalindrome('Madam'));               // false (case matters)
+console.log(isPalindrome("madam i'm adam"));      // false (all characters matter)
+console.log(isPalindrome('356653'));              // true
--- smallprobs/easy4/when_retire.js
+++ smallprobs/easy4/when_retire.js
@@ -1,19 +1,40 @@
 /*
-
+Build a program that logs when the user will retire and how many more years 
+the user has to work until retirement.
 */
 // TODO Problem
 /*
-INPUT = 
-OUTPUT = 
+INPUT = program asks user for age (number) and desired age (number) of retirement
+OUTPUT = logged string with projected year of retirement, with number of years
+until that year.
 */
 // TODO Examples
 /*
+What is your age? 30
+At what age would you like to retire? 70
 
+It's 2017. You will retire in 2057.
+You have only 40 years of work to go!
 */
 // TODO Data Structures
 // 
 // TODO Algorithm
 /*
-
+require readlinesync
+declare variable = ask user for age
+declare variable = ask user for age they'd like to retire
+declare variable for current year
+years left to work = desired age of retirement - current age
+console.log year, (yLTW + current year), and yLTW.
 */
 // TODO Code
+
+let readline = require("readline-sync");
+
+let currentAge = readline.question("What is your age? ");
+let retirementAge = readline.question("At what age would you like to retire? ");
+let currentYear = new Date().getFullYear();
+let yearsLeftToWork = retirementAge - currentAge;
+
+console.log(`It's ${currentYear}. You will retire in ${currentYear + yearsLeftToWork}.\n` +
+`You have only ${yearsLeftToWork} years of work to go!`);
