e59700b59cbbf4acb649e748a9498e5ba5b66ab9
Rees Dart <thedominicparker@gmail.com>
2020-11-26 20:07:05 -0800

    adding find_duplicates.js and combine_two_arrays.js problems and solutions.

--- smallprobs/easy5/combine_two_lists.js
+++ smallprobs/easy5/combine_two_lists.js
@@ -1,25 +1,50 @@
 /*
+Write a function that combines two arrays passed as arguments, and returns a 
+new array that contains all elements from both array arguments, with each 
+element taken in alternation.
+
+You may assume that both input arrays are non-empty, and that they have the 
+same number of elements.
 
 */
 // TODO Problem
 /*
 Explicit Requirements:
- - INPUT = 
- - OUTPUT = 
- - RULES =
+ - INPUT = two arrays
+ - OUTPUT = one new array, containing all the elements from the input arrays, however
+ it must take one element from argument array 1, then one element from argument array 2, 
+ then repeat this until all elements from both arrays have been used.
+ - RULES = alternation of elements. both arrays are the same length. 
 
 Implicit Requirements:
 
 Questions: 
+- does it matter which array is first?
 */
 // TODO Examples
 /*
-
+interleave([1, 2, 3], ['a', 'b', 'c']);    // [1, "a", 2, "b", 3, "c"]
 */
 // TODO Data Structures
-// 
+// arrays
 // TODO Algorithm
 /*
+ - function takes in two arrays as arguments.
+ - create an empty result array to put the elements in.
+ - for each element in both arrays, up to the length of one of the arrays, 
+  - push an element from array 1 into result array.
+  - push an element form array 2 into result array.
+ - return result array. 
 
 */
 // TODO Code
+
+let interleave = (array1, array2) => {
+  let result = [];
+  for (let i = 0; i < array1.length; i += 1) {
+    result.push(array1[i], array2[i]);
+  }
+  return result;
+}
+
+interleave([1, 2, 3], ['a', 'b', 'c']);    // [1, "a", 2, "b", 3, "c"]
--- smallprobs/easy5/find_the_duplicate.js
+++ smallprobs/easy5/find_the_duplicate.js
@@ -1,25 +1,99 @@
 /*
-
+Given an unordered array and the information that exactly one value in the 
+array occurs twice (every other value occurs exactly once), determine which 
+value occurs twice. Write a function that will find and return the duplicate 
+value that is in the array.
 */
 // TODO Problem
 /*
 Explicit Requirements:
- - INPUT = 
- - OUTPUT = 
- - RULES =
+ - INPUT = unordered array of values.
+ - OUTPUT = the duplicate value, returned.
+ - RULES = one value in the array occurs twice/ is a duplicate.
 
 Implicit Requirements:
-
+  array may include any type of value, primitive and not.
 Questions:  
 */
 // TODO Examples
 /*
-
+findDup([1, 5, 3, 1]);                                // 1
+findDup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+         38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+         14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+         78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+         89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+         41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+         55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+         85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+         40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+          7, 34, 57, 74, 45, 11, 88, 67,  5, 58]);    // 73
 */
 // TODO Data Structures
-// 
+// array is given
 // TODO Algorithm
 /*
+ - function takes an array of values.
+ - for each value in the array, 
+  - if value is not equal to any of the other values in the array, continue.
+  - but if the value is equal to any of the other values, return value.
 
 */
 // TODO Code
+
+function findDup(array) {
+  let rootComparisonElement;
+  for (let i = 0; i < array.length; i += 1) {
+    rootComparisonElement = array[i];
+    if (array.slice(i + 1).includes(rootComparisonElement)) break;
+  }
+  return rootComparisonElement;
+}
+
+findDup([1, 5, 3, 1]);                                // 1
+findDup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+         38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+         14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+         78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+         89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+         41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+         55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+         85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+         40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+          7, 34, 57, 74, 45, 11, 88, 67,  5, 58]);    // 73
+
+// another solution: 
+function findDup(array) {
+  let seen = {};
+
+  for (let idx = 0; idx < array.length; idx += 1) {
+    if (seen[array[idx]]) {
+      return array[idx];
+    } else {
+      seen[array[idx]] = true;
+    }
+  }
+
+  return undefined;
+}
+
+// and other solution: 
+
+function count(array, element) {
+  let count = 0;
+  for (let idx = 0; idx < array.length; idx++) {
+    if (array[idx] === element) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+function findDup(array) {
+  return array.find(element => count(array, element) === 2);
+}
+
+// this one loops through and with each iteration, the element at the current 
+// index is checked to see if it equals the current element passed to the 
+// count function. The duplicate will be the one that causes the count to return
+// as 2.
