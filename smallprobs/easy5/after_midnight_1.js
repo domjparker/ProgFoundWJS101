08927f22786642e36d466d55aa1cb5de67134ab8
Rees Dart <thedominicparker@gmail.com>
2020-12-01 09:10:07 -0800

    adding 2 problems and solutions, along with linter file.

--- smallprobs/easy5/after_midnight_1.js
+++ smallprobs/easy5/after_midnight_1.js
@@ -1,25 +1,74 @@
 /*
+The time of day can be represented as the number of minutes before or after 
+midnight. If the number of minutes is positive, the time is after midnight. If 
+the number of minutes is negative, the time is before midnight.
 
+Write a function that takes a time using this minute-based format and returns 
+the time of day in 24 hour format (hh:mm). Your method should work with any 
+integer input.
 */
 // TODO Problem
 /*
 Explicit Requirements:
- - INPUT = 
- - OUTPUT = 
- - RULES =
+ - INPUT = integer
+ - OUTPUT = string of numbers representing hours and minutes, with colon between
+ - RULES = result is the time of day in 24 hour format. Cannot use js 'Date'
+ class methods. Daylight savings and other complications don't apply.
 
 Implicit Requirements:
-
+- result must be four digits, including preceding zeros. can take positive and
+negative integers.
 Questions: 
 */
 // TODO Examples
 /*
-
+console.log(timeOfDay(0) === "00:00");
+console.log(timeOfDay(-3) === "23:57");
+console.log(timeOfDay(35) === "00:35");
+console.log(timeOfDay(-1437) === "00:03");
+console.log(timeOfDay(3000) === "02:00");
+console.log(timeOfDay(800) === "13:20");
+console.log(timeOfDay(-4231) === "01:29");
 */
 // TODO Data Structures
-// 
+// integers
 // TODO Algorithm
 /*
-
+ -function takes an integer
+ - 60 minutes in an hour
+ - 24 hours in a day.
+ - divide by 'minutes in an hour' to get amount of hours
+ - remainder by 'minutes in an hour' to get amount of minutes.
+ - if amount of hours is greater than 'dayHours'
+    - remainder by dayHours to get 'hours'
+ - if integer is positive:
+ - if integer is negative: 
+  - subtract hours from dayHours.
+- return string literal of hours colon minutes.
 */
 // TODO Code
+const MINS_IN_HOUR = 60;
+const HOURS_IN_DAY = 24;
+
+function timeOfDay(number) {
+  let hoursAmount = Math.floor(number / MINS_IN_HOUR);
+  let minutesAmount = number % MINS_IN_HOUR;
+  if (hoursAmount % HOURS_IN_DAY !== 0) {
+    hoursAmount %= HOURS_IN_DAY;
+  }
+  if (Math.sign(number) === -1) {
+    hoursAmount = HOURS_IN_DAY + hoursAmount;
+    minutesAmount = MINS_IN_HOUR + minutesAmount;
+  }
+  let finalHours = hoursAmount.toString().padStart(2, "0");
+  let finalMinutes = minutesAmount.toString().padStart(2, "0");
+  return finalHours + ":" + finalMinutes;
+}
+
+console.log(timeOfDay(0) === "00:00");
+console.log(timeOfDay(-3) === "23:57");
+console.log(timeOfDay(35) === "00:35");
+console.log(timeOfDay(-1437) === "00:03");
+console.log(timeOfDay(3000) === "02:00");
+console.log(timeOfDay(800) === "13:20");
+console.log(timeOfDay(-4231) === "01:29");
--- smallprobs/easy5/array_average.js
+++ smallprobs/easy5/array_average.js
@@ -1,25 +1,54 @@
 /*
-
+Write a function that takes one argument, an array of integers, and returns the
+average of all the integers in the array, rounded down to the integer component
+of the average. The array will never be empty, and the numbers will always be 
+positive integers.
 */
 // TODO Problem
 /*
 Explicit Requirements:
- - INPUT = 
- - OUTPUT = 
- - RULES =
+ - INPUT = array of integers
+ - OUTPUT = the average of all the integers, rounded down to the integer component
+ of the average.
+ - RULES = must be rounded down after averaging.
 
 Implicit Requirements:
-
+ - average > numbers summed and divided by amount of elements in the array.
 Questions: 
-*/
+ - 
+*/  
 // TODO Examples
 /*
-
+average([1, 5, 87, 45, 8, 8]);       // 25
+average([9, 47, 23, 95, 16, 52]);    // 40
 */
 // TODO Data Structures
-// 
+// array > number
 // TODO Algorithm
 /*
-
+- function takes in array of integers
+- add each element together to get single integer.
+- divide single integer by the length of the array.
+- round result down to nearest whole integer
+- return result
 */
 // TODO Code
+
+function average(array) {
+  let arraySummed = array.reduce((acc, curr) => {
+    return acc + curr
+  }, 0);
+  let sumAveraged = arraySummed / array.length;
+  let averageRoundedDown = Math.floor(sumAveraged);
+  return averageRoundedDown;
+}
+
+// or
+
+function average(array) {
+  return Math.floor(array.reduce((acc, curr) => acc + curr, 0) / array.length);
+}
+
+
+average([1, 5, 87, 45, 8, 8]);       // 25
+average([9, 47, 23, 95, 16, 52]);    // 40
