789d3e9b012ccbb29afe78640981997c9e3d525d
Rees Dart <thedominicparker@gmail.com>
2020-12-02 08:58:10 -0800

    adding solution in reverse_number.js

--- smallprobs/easy6/reverse_number.js
+++ smallprobs/easy6/reverse_number.js
@@ -1,12 +1,13 @@
 /*
-
+Write a function that takes a positive integer as an argument, and returns that
+number with its digits reversed.
 */
 // TODO Problem
 /*
 Explicit Requirements:
- - INPUT =
- - OUTPUT =
- - RULES =
+ - INPUT = positive integer
+ - OUTPUT = integer with individual digits of input integer reversed
+ - RULES = leading zeros in result get dropped.
 
 Implicit Requirements:
 
@@ -15,14 +16,36 @@ Questions:
 */
 // TODO Examples
 /*
-
+reverseNumber(12345);    // 54321
+reverseNumber(12213);    // 31221
+reverseNumber(456);      // 654
+reverseNumber(12000);    // 21 -- Note that leading zeros in the result get
+dropped!
+reverseNumber(1);        // 1
 */
 // TODO Data Structures
 /*
-
+number > string > reverse > number
 */
 // TODO Algorithm
 /*
-
+ - function takes in a number
+ - convert number to array
+ - reverse array
+ - join array into string and convert to number
+ - return result
 */
 // TODO Code
+
+let reverseNumber = num => {
+  let numArray = Array.from(num.toString());
+  let numArrayReversed = numArray.reverse();
+  let numReversed = Number(numArrayReversed.join(''));
+  return numReversed;
+};
+
+reverseNumber(12345);    // 54321
+reverseNumber(12213);    // 31221
+reverseNumber(456);      // 654
+reverseNumber(12000);    // 21 -- Note that leading zeros in the result get dropped!
+reverseNumber(1);        // 1
