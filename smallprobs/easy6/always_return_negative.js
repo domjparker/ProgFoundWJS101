69b5c0108a97bdcdfa2893814a72402c6f65f34a
Rees Dart <thedominicparker@gmail.com>
2020-12-02 10:13:58 -0800

    adding solution for counting_up.js

--- smallprobs/easy6/always_return_negative.js
+++ smallprobs/easy6/always_return_negative.js
@@ -42,3 +42,12 @@ function negative(num) {
 negative(5);     // -5
 negative(-3);    // -3
 negative(0);     // -0
+
+
+function negative(num) {
+  return (num < 0) ? num : num * -1;
+}
+
+negative(5);     // -5
+negative(-3);    // -3
+negative(0);     // -0
--- smallprobs/easy6/counting_up.js
+++ smallprobs/easy6/counting_up.js
@@ -1,12 +1,13 @@
 /*
-
+Write a function that takes an integer argument, and returns an array containing
+all integers between 1 and the argument (inclusive), in ascending order.
 */
 // TODO Problem
 /*
 Explicit Requirements:
- - INPUT =
- - OUTPUT =
- - RULES =
+ - INPUT = integer
+ - OUTPUT = array containing all integers from 1 to the argument
+ - RULES = integer will always be positive.
 
 Implicit Requirements:
 
@@ -15,14 +16,34 @@ Questions:
 */
 // TODO Examples
 /*
-
+sequence(5);    // [1, 2, 3, 4, 5]
+sequence(3);    // [1, 2, 3]
+sequence(1);    // [1]
 */
 // TODO Data Structures
 /*
-
+number to array
 */
 // TODO Algorithm
 /*
-
+ - function takes in number
+ - create empty result array
+ - iterative loop up to number
+  - add counter to array
+ - return array
 */
 // TODO Code
+
+function sequence(num) {
+  let resultArray = [];
+  let counter = 1;
+  do {
+    resultArray.push(counter);
+    counter += 1;
+  } while (counter <= num);
+  return resultArray;
+}
+
+sequence(5);    // [1, 2, 3, 4, 5]
+sequence(3);    // [1, 2, 3]
+sequence(1);    // [1]
