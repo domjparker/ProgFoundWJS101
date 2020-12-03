eb2646d752e7733fb89112a54b7228de178f74a8
Rees Dart <thedominicparker@gmail.com>
2020-12-03 09:36:45 -0800

    adding 2 problems and solutions.

--- smallprobs/easy6/reverse_it_1.js
+++ smallprobs/easy6/reverse_it_1.js
@@ -1,28 +1,43 @@
 /*
-
+Write a function that takes a string argument, and returns a new string
+containing the words from the string argument in reverse order.
 */
 // TODO Problem
 /*
 Explicit Requirements:
- - INPUT =
- - OUTPUT =
- - RULES =
+ - INPUT = string
+ - OUTPUT = new string
+ - RULES = any words in the new string should be reversed
 
 Implicit Requirements:
-
+ - words would be collections of characters separated by spaces
 Questions:
 
 */
 // TODO Examples
 /*
-
+reverseSentence('');                       // ""
+reverseSentence('Hello World');            // "World Hello"
+reverseSentence('Reverse these words');    // "words these Reverse"
 */
 // TODO Data Structures
 /*
-
+string > array > reverse > string
 */
 // TODO Algorithm
 /*
-
+ - function takes in string.
+ - split string into words separated by spaces.
+ - reverse array.
+ - join array into string.
+ - return string.
 */
 // TODO Code
+
+function reverseSentence(string) {
+  return string.split(' ').reverse().join(' ');
+}
+
+reverseSentence('');                       // ""
+reverseSentence('Hello World');            // "World Hello"
+reverseSentence('Reverse these words');    // "words these Reverse"
--- smallprobs/easy6/sequence_count.js
+++ smallprobs/easy6/sequence_count.js
@@ -1,12 +1,23 @@
 /*
+Create a function that takes two integers as arguments. The first argument is a
+count, and the second is the starting number of a sequence that your function
+will create. The function should return an array containing the same number of
+elements as the count argument. The value of each element should be a multiple
+of the starting number.
 
+You may assume that the count argument will always be an integer greater than
+or equal to 0. The starting number can be any integer. If the count is 0, the
+function should return an empty array.
 */
 // TODO Problem
 /*
 Explicit Requirements:
- - INPUT =
- - OUTPUT =
- - RULES =
+ - INPUT = 2 integers
+ - OUTPUT = array of integers
+ - RULES = first integer is a count integer, second integers is start of a
+ sequence. should result with an array of integers, the first of which is the
+ 2nd input argument integer, and each consecutive integer should be a result of
+ multiplying that number by the sequence of integers leading up the count number
 
 Implicit Requirements:
 
@@ -15,14 +26,41 @@ Questions:
 */
 // TODO Examples
 /*
-
+sequence(5, 1);          // [1, 2, 3, 4, 5]
+sequence(4, -7);         // [-7, -14, -21, -28]
+sequence(3, 0);          // [0, 0, 0]
+sequence(0, 1000000);    // []
 */
 // TODO Data Structures
 /*
-
+array of integers
 */
 // TODO Algorithm
 /*
-
+ - function takes in count integer and start integer.
+ - declare array empty
+ - iterative loop
+  - each iteration, add index/counter multiplied by start to array.
+ - return array.
 */
 // TODO Code
+
+function sequence(countNum, startNum) {
+  let resultArray = [];
+  let counter = 1;
+  while (counter <= countNum) {
+    resultArray.push(counter * startNum);
+    counter += 1;
+  }
+  return resultArray;
+}
+
+sequence(5, 1);          // [1, 2, 3, 4, 5]
+sequence(4, -7);         // [-7, -14, -21, -28]
+sequence(3, 0);          // [0, 0, 0]
+sequence(0, 1000000);    // []
+
+// or an interesting one
+function sequence(count, startingNum) {
+  return Array.from(Array(count).keys(), num => (num + 1) * startingNum); 
+};
