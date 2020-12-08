01da2077ed0f89b65ccca541a68f60fb38a02784
Rees Dart <thedominicparker@gmail.com>
2020-12-08 07:37:26 -0800

    adding first file for lesson4.

---
+++ lesson4/selection_and_transformation.js
@@ -0,0 +1,99 @@
+// Selection from an array of objects
+let produce = {
+  apple: 'Fruit',
+  carrot: 'Vegetable',
+  pear: 'Fruit',
+  broccoli: 'Vegetable'
+};
+
+let selectFruit = itemsList => {
+  let fruitItems = {}; // create empty object
+  let objectKeys = Object.keys(itemsList); // create an array of listObject's keys
+  for (let i = 0; i < objectKeys.length; i += 1) { // loop the length of objectKeys
+    if (itemsList[objectKeys[i]] === 'Fruit') { // if value at listObject at key index is fruit
+      fruitItems[objectKeys[i]] = itemsList[objectKeys[i]]; // write value as an object into fruitItems
+    }
+  }
+  return fruitItems;
+};
+
+selectFruit(produce); // => { apple: 'Fruit', pear: 'Fruit' }
+
+// =============================
+// Mutating/transforming an array with primitive elements
+
+function doubleNumbers(numbers) {
+  let counter = 0;
+
+  while (counter < numbers.length) {
+    // let currentNum = numbers[counter];
+    // currentNum *= 2;    ====== (this doesn't work because by creating a new variable,
+    // you're not mutating the actual value in numbers.)
+    numbers[counter] *= 2; // this works on each actual numbers value
+
+    counter += 1;
+  }
+
+  return numbers;
+}
+
+let myNumbers = [1, 4, 3, 7, 2, 6];
+doubleNumbers(myNumbers); // => [2, 8, 6, 14, 4, 12]
+myNumbers;                // => [2, 8, 6, 14, 4, 12]
+
+// or...
+
+function doubleNumbers(numbers) {
+  let counter = 0;
+
+  while (counter < numbers.length) {
+    let currentNum = numbers[counter];
+    numbers.splice(counter, 1, currentNum *= 2);
+    console.log(numbers)
+
+    counter += 1;
+  }
+
+  return numbers;
+}
+
+let myNumbers = [1, 4, 3, 7, 2, 6];
+doubleNumbers(myNumbers); // => [2, 8, 6, 14, 4, 12]
+myNumbers;                // => [2, 8, 6, 14, 4, 12]
+
+// ====================== 
+// transforming the numbers based on their position in the array
+
+function doubleEverySecondNumber(numbers) {
+  for (let counter = 0; counter < numbers.length; counter += 1) {
+    if (counter % 2 === 1) {
+      numbers[counter] *= 2;
+    }
+  }
+
+  return numbers;
+}
+
+let myNumbers = [1, 4, 3, 7, 2, 6];
+doubleEverySecondNumber(myNumbers);  // => [1, 4, 3, 7, 2, 6]
+
+// not mutated
+myNumbers;                    // => [1, 8, 3, 14, 2, 12]
+
+// =======================
+// function to take an additional argument in order to determine the transformation criterion
+
+function multiply(numbersArray, multiplier) {
+  let counter = 0;
+
+  while (counter < numbersArray.length) {
+    numbersArray[counter] *= multiplier;
+
+    counter += 1;
+  }
+
+  return numbersArray;
+}
+
+let myNumbers = [1, 4, 3, 7, 2, 6];
+multiply(myNumbers, 3); // => [3, 12, 9, 21, 6, 18]
--- smallprobs/easy6/matching_parentheses.js
+++ smallprobs/easy6/matching_parentheses.js
@@ -1,28 +1,104 @@
 /*
-
+Write a function that takes a string as argument, and returns true if
+all parentheses in the string are properly balanced, false otherwise.
+To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
 */
 // TODO Problem
 /*
 Explicit Requirements:
- - INPUT =
- - OUTPUT =
- - RULES =
+ - INPUT = string
+ - OUTPUT = boolean
+ - RULES = boolean result is true if there is the number of '(' matches the
+  number of ')'
 
 Implicit Requirements:
+ - can't start on a ) or end on a (
 
 Questions:
 
 */
 // TODO Examples
 /*
-
+console.log(isBalanced("What (is) this?") === true);
+console.log(isBalanced("What is) this?") === false);
+console.log(isBalanced("What (is this?") === false);
+console.log(isBalanced("((What) (is this))?") === true);
+console.log(isBalanced("((What)) (is this))?") === false);
+console.log(isBalanced("Hey!") === true);
+console.log(isBalanced(")Hey!(") === false);
+console.log(isBalanced("What ((is))) up(") === false);
 */
 // TODO Data Structures
 /*
-
+string (could do array)
 */
 // TODO Algorithm
 /*
-
+ - function takes in string
+ - set counter for left parenthesis symbol
+ - set counter for right parenthesis symbol
+ - check if first string or last string is wrong parenthesis
+ - for each character in string
+  - if left parenthesis, increment left parenthesis counter
+  - if right parenthesis, increment right parenthesis counter
+ if both counters are equal return true
+ if both counters are not equal return false
 */
 // TODO Code
+
+function isBalanced(string) {
+  let leftParenthesisCounter = 0;
+  let rightParenthesisCounter = 0;
+  for (let i = 0; i < string.length; i += 1) {
+    if (string[i] === "(") {
+      leftParenthesisCounter += 1;
+      if (leftParenthesisCounter < rightParenthesisCounter) return false;
+    } else if (string[i] === ")") {
+      rightParenthesisCounter += 1;
+      if (rightParenthesisCounter > leftParenthesisCounter) return false;
+    }
+  }
+  if (leftParenthesisCounter !== rightParenthesisCounter) return false;
+  else return true;
+}
+
+console.log(isBalanced("What (is) this?") === true);
+console.log(isBalanced("What is) this?") === false);
+console.log(isBalanced("What (is this?") === false);
+console.log(isBalanced("((What) (is this))?") === true);
+console.log(isBalanced("((What)) (is this))?") === false);
+console.log(isBalanced("Hey!") === true);
+console.log(isBalanced(")Hey!(") === false);
+console.log(isBalanced("What ((is))) up(") === false);
+
+// more efficient way...
+
+function isBalanced(string) {
+  let parenthesisCounter = 0;
+  for (let i = 0; i < string.length; i += 1) {
+    if (string[i] === "(") {
+      parenthesisCounter += 1;
+    } else if (string[i] === ")") {
+      parenthesisCounter -= 1;
+    }
+    if (parenthesisCounter < 0) return false;
+  }
+  return parenthesisCounter === 0;
+}
+
+console.log(isBalanced("What (is) this?") === true);
+console.log(isBalanced("What is) this?") === false);
+console.log(isBalanced("What (is this?") === false);
+console.log(isBalanced("((What) (is this))?") === true);
+console.log(isBalanced("((What)) (is this))?") === false);
+console.log(isBalanced("Hey!") === true);
+console.log(isBalanced(")Hey!(") === false);
+console.log(isBalanced("What ((is))) up(") === false);
+
+// or...
+ß
+function isBalanced(str) {
+  let parensStr = str.match(/[()]/g).join('');
+  while (parensStr.includes('()')) parensStr = parensStr.replace('()', '');
+  return !parensStr;
+}
