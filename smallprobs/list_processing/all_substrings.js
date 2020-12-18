f929b2cb0f97c310af8dde6bf6b2ed16b00ef92b
Rees Dart <thedominicparker@gmail.com>
2020-12-18 08:01:45 -0800

    adding solutions for all_substrings.js and leading_substrings.js

--- smallprobs/list_processing/all_substrings.js
+++ smallprobs/list_processing/all_substrings.js
@@ -1,12 +1,13 @@
 /*
+Write a function that returns a list of all substrings of a string. Order the returned list by where in the string the substring begins. This means that all substrings that start at position 0 should come first, then all substrings that start at position 1, and so on. Since multiple substrings will occur at each position, return the substrings at a given position from shortest to longest.
 
 */
 // TODO Problem
 /*
 Explicit Requirements:
- - INPUT =
- - OUTPUT =
- - RULES =
+- INPUT = string
+- OUTPUT = list of all possible substring combinations
+- RULES = substrings must be ordered alphabetically, according to the first letter in them, and according to substring length.
 
 Implicit Requirements:
 
@@ -15,14 +16,49 @@ Questions:
 */
 // TODO Examples
 /*
+substrings('abcde');
 
+returns
+[ "a", "ab", "abc", "abcd", "abcde",
+  "b", "bc", "bcd", "bcde",
+  "c", "cd", "cde",
+  "d", "de",
+  "e" ]
 */
 // TODO Data Structures
 /*
-
+string > array
 */
 // TODO Algorithm
 /*
+-function takes in string
+-let empty result array
 -
 */
 // TODO Code
+
+function leadingSubstrings(string) {
+  let result = [];
+  for (let length = 1; length <= string.length; length += 1) {
+    result.push(string.slice(0, length))
+  }
+  return result
+}
+
+function substrings(string) {
+  let finalResult = [];
+  for (let i = 0; i < string.length; i += 1) {
+    finalResult.push(leadingSubstrings(string.slice(i)));
+  }
+
+  return finalResult.flat();
+}
+
+substrings('abcde');
+
+// returns
+// [ "a", "ab", "abc", "abcd", "abcde",
+//   "b", "bc", "bcd", "bcde",
+//   "c", "cd", "cde",
+//   "d", "de",
+//   "e" ]
--- smallprobs/list_processing/leading_substrings.js
+++ smallprobs/list_processing/leading_substrings.js
@@ -1,28 +1,90 @@
 /*
-
+Write a function that takes a string argument, and returns a list of all substrings that start from the beginning of the string, ordered from shortest to longest.
 */
 // TODO Problem
 /*
 Explicit Requirements:
- - INPUT =
- - OUTPUT =
- - RULES =
+- INPUT = string argument
+- OUTPUT = list of all substrings that start from the beginning of the string, from shortest to longest
+- RULES = 
 
 Implicit Requirements:
+-list returned as array
 
 Questions:
+-what about spaces?
 
 */
 // TODO Examples
 /*
-
+leadingSubstrings('abc');      // ["a", "ab", "abc"]
+leadingSubstrings('a');        // ["a"]
+leadingSubstrings('xyzzy');    // ["x", "xy", "xyz", "xyzz", "xyzzy"]
 */
 // TODO Data Structures
 /*
-
+string > array > map
 */
 // TODO Algorithm
 /*
--
+-function takes in string.
+-set arrayResult= [];
+-set stringAccumulator = ""
+-string split into array.
+-for each element in array
+  -stringAccumulator = stringAccumulator + element
+  -push to arrayResult
+-return arrayResult;
+
+or: 
+-function takes in string.
+-set stringAccumulator = ""
+-string split into array.
+-map each element in array
+  -stringAccumulator = stringAccumulator + element
+  return stringAccumulator;
+-return arrayResult;
 */
 // TODO Code
+
+function leadingSubstrings(string) {
+  let arrayResult = [];
+  let stringAccumulator = "";
+  string.split("").forEach(element => {
+    stringAccumulator += element;
+    arrayResult.push(stringAccumulator);
+  })
+  return arrayResult;
+}
+
+leadingSubstrings('abc');      // ["a", "ab", "abc"]
+leadingSubstrings('a');        // ["a"]
+leadingSubstrings('xyzzy');    // ["x", "xy", "xyz", "xyzz", "xyzzy"]
+
+// or...
+
+function leadingSubstrings(string) {
+  let stringAccumulator = "";
+  return string.split("").map(element => {
+    return stringAccumulator += element;
+  });
+}
+
+leadingSubstrings('abc');      // ["a", "ab", "abc"]
+leadingSubstrings('a');        // ["a"]
+leadingSubstrings('xyzzy');    // ["x", "xy", "xyz", "xyzz", "xyzzy"]
+
+// or...
+
+function leadingSubstrings(string) {
+  let result = [];
+  for (let length = 1; length <= string.length; length += 1) {
+    result.push(string.slice(0, length))
+  }
+
+  return result;
+}
+
+leadingSubstrings('abc');      // ["a", "ab", "abc"]
+leadingSubstrings('a');        // ["a"]
+leadingSubstrings('xyzzy');    // ["x", "xy", "xyz", "xyzz", "xyzzy"]
