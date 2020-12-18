776a8c10f3b90c8764a2d6a1bb40befd41e83ff3
Rees Dart <thedominicparker@gmail.com>
2020-12-18 10:23:16 -0800

    adding palindromic_substrings.js and sum_of_nums.js.

--- smallprobs/list_processing/palindromic_substrings.js
+++ smallprobs/list_processing/palindromic_substrings.js
@@ -1,28 +1,92 @@
 /*
+Write a function that returns a list of all palindromic substrings of a string. That is, each substring must consist of a sequence of characters that reads the same forward and backward. The substrings in the returned list should be sorted by their order of appearance in the input string. Duplicate substrings should be included multiple times.
 
+You may (and should) use the substrings function you wrote in the previous exercise.
+
+For the purpose of this exercise, you should consider all characters and pay attention to case; that is, 'AbcbA' is a palindrome, but 'Abcba' and 'Abc-bA' are not. In addition, assume that single characters are not palindromes.
 */
 // TODO Problem
 /*
 Explicit Requirements:
- - INPUT =
- - OUTPUT =
- - RULES =
+- INPUT = string
+- OUTPUT = list of all palindromic (read same when forwards or backwards) substrings in input string
+- RULES = single characters are not palindromes. characters are case sensitive.
 
 Implicit Requirements:
 
 Questions:
-
+- ignore empty spaces?
 */
 // TODO Examples
 /*
+palindromes('abcd');       // []
+palindromes('madam');      // [ "madam", "ada" ]
 
+palindromes('hello-madam-did-madam-goodbye');
+// returns
+// [ "ll", "-madam-", "-madam-did-madam-", "madam", "madam-did-madam", "ada",
+//   "adam-did-mada", "dam-did-mad", "am-did-ma", "m-did-m", "-did-", "did",
+//   "-madam-", "madam", "ada", "oo" ]
+
+palindromes('knitting cassettes');
+// returns
+// [ "nittin", "itti", "tt", "ss", "settes", "ette", "tt" ]
 */
 // TODO Data Structures
 /*
-
+string > array of all substrings > check palindromes > sort.
 */
 // TODO Algorithm
 /*
--
+-function takes in string.
+-get array of all possible substrings
+-filter substrings array for palindromes
+  -if substring length is greater than 1
+    -return true if substring is equal to substring reversed
+-return filtered array.
 */
 // TODO Code
+
+
+function leadingSubstrings(string) {
+  let result = [];
+  for (let length = 1; length <= string.length; length += 1) {
+    result.push(string.slice(0, length))
+  }
+  return result
+}
+
+function substrings(string) {
+  let finalResult = [];
+  for (let i = 0; i < string.length; i += 1) {
+    finalResult.push(leadingSubstrings(string.slice(i)));
+  }
+
+  return finalResult.flat();
+}
+
+function palindromes(string) {
+  let allSubstrings = substrings(string);
+  return allSubstrings.filter(substring => {
+    if (substring.length > 1) {
+      return (substring === substring.split('').reverse().join(''));
+    }
+  })
+}
+
+palindromes('abcd');
+palindromes('madam');
+palindromes('hello-madam-did-madam-goodbye');
+palindromes('knitting cassettes');
+
+
+
+// or...
+
+function isPalindrome(word) {
+  return word.length > 1 && word === word.split("").reverse().join("");
+}
+          
+function palindromes(string) {
+  return substrings(string).filter(isPalindrome);
+}
--- smallprobs/list_processing/sum_of_sums.js
+++ smallprobs/list_processing/sum_of_sums.js
@@ -1,12 +1,12 @@
 /*
-
+Write a function that takes an array of numbers, and returns the sum of the sums of each leading subsequence for that array. You may assume that the array always contains at least one number.
 */
 // TODO Problem
 /*
 Explicit Requirements:
- - INPUT =
- - OUTPUT =
- - RULES =
+- INPUT = array of numbers
+- OUTPUT = 
+- RULES = the input array will not be empty
 
 Implicit Requirements:
 
@@ -15,14 +15,78 @@ Questions:
 */
 // TODO Examples
 /*
-
+sumOfSums([3, 5, 2]);        // (3) + (3 + 5) + (3 + 5 + 2) --> 21
+sumOfSums([1, 5, 7, 3]);     // (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
+sumOfSums([4]);              // 4
+sumOfSums([1, 2, 3, 4, 5]);  // 35
 */
 // TODO Data Structures
 /*
-
+array of nums > number
 */
 // TODO Algorithm
 /*
--
+-function takes in array of nums
+-set workingSum = 0.
+-set workingArray = []
+-for the length of the array
+  -push workingSum + current number in iteration into workingArray
+-reduce working array
+-return result 
 */
 // TODO Code
+
+function sumOfSums(inputArray) {
+  let workingSum = 0;
+  let workingArray = [];
+  for (let i = 0; i < inputArray.length; i += 1) {
+    workingArray.push(workingSum + inputArray[i]);
+    workingSum += inputArray[i];
+  }
+  return workingArray.reduce((acc, curr) => acc + curr);
+}
+
+
+sumOfSums([3, 5, 2]);        // (3) + (3 + 5) + (3 + 5 + 2) --> 21
+sumOfSums([1, 5, 7, 3]);     // (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
+sumOfSums([4]);              // 4
+sumOfSums([1, 2, 3, 4, 5]);  // 35
+
+// or....
+
+function sumOfSums(numbers) {
+  let sumTotal = 0;
+  for (let idx = 1; idx <= numbers.length; idx++) {
+    sumTotal += numbers.slice(0, idx).reduce((accum, num) => accum + num);
+  }
+  return sumTotal;
+}
+
+// above solution is better, but this is just different:
+
+function sumOfSums(numbers) {
+  let sumTotal = 0;
+  numbers = numbers.reverse();
+  for (let idx = 0; idx < numbers.length; idx += 1) {
+    sumTotal += numbers.slice(idx).reduce((accum, num) => accum + num);
+  }
+  return sumTotal;
+}
+
+sumOfSums([3, 5, 2]);        // (3) + (3 + 5) + (3 + 5 + 2) --> 21
+sumOfSums([1, 5, 7, 3]);     // (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
+sumOfSums([4]);              // 4
+sumOfSums([1, 2, 3, 4, 5]);  // 35
+
+// also:
+
+function sumOfSums(numbers) {
+  return numbers.map((_, index) => {
+    return numbers.slice(0, index + 1).reduce((acc, curr) => acc + curr)  
+  }).reduce((acc, curr) => acc + curr);
+}
+
+sumOfSums([3, 5, 2]);        // (3) + (3 + 5) + (3 + 5 + 2) --> 21
+sumOfSums([1, 5, 7, 3]);     // (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
+sumOfSums([4]);              // 4
+sumOfSums([1, 2, 3, 4, 5]);  // 35
