99bf09e40c015b93962c509ec4af4039627731e2
Rees Dart <thedominicparker@gmail.com>
2020-12-21 06:29:09 -0800

    adding 2 problems/solutions.

--- smallprobs/string_and_text_processing/delete_vowels.js
+++ smallprobs/string_and_text_processing/delete_vowels.js
@@ -1,28 +1,77 @@
 /*
-
+Write a function that takes an array of strings, and returns an array of the same values with all vowels (a, e, i, o, u) removed.
 */
 // TODO Problem
 /*
 Explicit Requirements:
-- INPUT =
-- OUTPUT =
-- RULES =
+- INPUT = array of strings
+- OUTPUT = array of input with vowels removed
+- RULES = 
 
 Implicit Requirements:
 
 Questions:
-
+- can it be the same array, or a should it be a new array?
 */
 // TODO Examples
 /*
-
+removeVowels(['abcdefghijklmnopqrstuvwxyz']);         // ["bcdfghjklmnpqrstvwxyz"]
+removeVowels(['green', 'YELLOW', 'black', 'white']);  // ["grn", "YLLW", "blck", "wht"]
+removeVowels(['ABC', 'AEIOU', 'XYZ']);                // ["BC", "", "XYZ"]
 */
 // TODO Data Structures
 /*
-
+arrays
 */
 // TODO Algorithm
 /*
-
+function takes in array of strings
+create new array
+for each string element
+  remove vowel characters
+  push string to new array
+return new array
 */
 // TODO Code
+
+function removeVowels(arrayOfStrings) {
+  let finalArray = [];
+  arrayOfStrings.forEach(stringElement => {
+    finalArray.push(stringElement.replace(/[aeiou]/gi, ''))
+  })
+  return finalArray;
+}
+
+removeVowels(['abcdefghijklmnopqrstuvwxyz']);         // ["bcdfghjklmnpqrstvwxyz"]
+removeVowels(['green', 'YELLOW', 'black', 'white']);  // ["grn", "YLLW", "blck", "wht"]
+removeVowels(['ABC', 'AEIOU', 'XYZ']);                // ["BC", "", "XYZ"]
+
+
+// but this is better ...
+
+function removeVowels(arrayOfStrings) {
+  return arrayOfStrings.map(stringElement => {
+    return stringElement.replace(/[aeiou]/gi, '')
+  })
+}
+
+// or ....
+
+function removeVowels(stringArray) {
+  return stringArray.map(string => {
+    let chars = string.split("");
+    let removedVowels = deleteVowels(chars);
+    return removedVowels.join("");
+  });
+}
+
+function deleteVowels(array) {
+  const VOWELS = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  return array.map(char => {
+    if (VOWELS.indexOf(char) >= 0) {
+      return "";
+    } else {
+      return char;
+    }
+  });
+}
--- smallprobs/string_and_text_processing/uppercase_check.js
+++ smallprobs/string_and_text_processing/uppercase_check.js
@@ -1,12 +1,12 @@
 /*
-
+Write a function that takes a string argument, and returns true if all of the alphabetic characters inside the string are uppercase; false otherwise. Ignore characters that are not alphabetic.
 */
 // TODO Problem
 /*
 Explicit Requirements:
-- INPUT =
-- OUTPUT =
-- RULES =
+- INPUT = string of characters
+- OUTPUT = boolean, based on whether all alphabetic characters in the string are upper case.
+- RULES = non-alphabetic characters can be ignored.
 
 Implicit Requirements:
 
@@ -15,14 +15,34 @@ Questions:
 */
 // TODO Examples
 /*
-
+isUppercase('t');               // false
+isUppercase('T');               // true
+isUppercase('Four Score');      // false
+isUppercase('FOUR SCORE');      // true
+isUppercase('4SCORE!');         // true
+isUppercase('');                // true
 */
 // TODO Data Structures
 /*
-
+string > array > check each > boolean
 */
 // TODO Algorithm
 /*
+function takes in string
+check string for lower case characters, using match and regex
+if array isn't empty, true. othewise false.
 
 */
 // TODO Code
+
+let isUppercase = string => {
+  return (string.match(/[a-z]/g) === null);
+}
+
+
+isUppercase('t');               // false
+isUppercase('T');               // true
+isUppercase('Four Score');      // false
+isUppercase('FOUR SCORE');      // true
+isUppercase('4SCORE!');         // true
+isUppercase('');                // true
