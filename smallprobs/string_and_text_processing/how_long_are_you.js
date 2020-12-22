6337eb80e36a27ae12c6f214be6657af64213009
Rees Dart <thedominicparker@gmail.com>
2020-12-22 06:47:14 -0800

    adding 2 solutions.

--- smallprobs/string_and_text_processing/how_long_are_you.js
+++ smallprobs/string_and_text_processing/how_long_are_you.js
@@ -1,12 +1,13 @@
 /*
-
+Write a function that takes a string as an argument, and returns an array that contains every word from the string, with each word followed by a space and the word's length. If the argument is an empty string or if no argument is passed, the function should return an empty array.
+Assume that every pair of words in the string will be separated by a single space.
 */
 // TODO Problem
 /*
 Explicit Requirements:
-- INPUT =
-- OUTPUT =
-- RULES =
+- INPUT = string
+- OUTPUT = array containing every word from the string trailed by a space and then a number (still part of string)
+- RULES = if argument is empty string, or if there is not argument used, return empty array.
 
 Implicit Requirements:
 
@@ -15,14 +16,67 @@ Questions:
 */
 // TODO Examples
 /*
+wordLengths('cow sheep chicken');
+// ["cow 3", "sheep 5", "chicken 7"]
 
+wordLengths('baseball hot dogs and apple pie');
+// ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+wordLengths("It ain't easy, is it?");
+// ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+wordLengths('Supercalifragilisticexpialidocious');
+// ["Supercalifragilisticexpialidocious 34"]
+
+wordLengths('');      // []
+wordLengths();        // []
 */
 // TODO Data Structures
 /*
-
+string > array of strings
 */
 // TODO Algorithm
 /*
+function takes in string
+split string into array of strings
+if stringArray not [], 
+  for each string in array
+    return string + _ + string.length
+else return []
+return result
 
 */
 // TODO Code
+
+function wordLengths(string = "") {
+  if (string !== "") {
+    let stringArray = string.split(" ");
+    return stringArray.map(word => `${word} ${String(word.length)}`);
+  } else return [];
+}
+
+// function wordLengths(string = "") {
+//   let newArray = []
+//   if (string !== "") {
+//     let stringArray = string.split(" ");
+//     for (let i = 0; i < stringArray.length; i += 1) {
+//       newArray.push(`${stringArray[i]} ${stringArray[i].length}`);
+//     } 
+//   }
+//   return newArray;
+// }
+
+wordLengths('cow sheep chicken');
+// ["cow 3", "sheep 5", "chicken 7"]
+
+wordLengths('baseball hot dogs and apple pie');
+// ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+wordLengths("It ain't easy, is it?");
+// ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+wordLengths('Supercalifragilisticexpialidocious');
+// ["Supercalifragilisticexpialidocious 34"]
+
+wordLengths('');      // []
+wordLengths();        // []
--- smallprobs/string_and_text_processing/staggered_caps_2.js
+++ smallprobs/string_and_text_processing/staggered_caps_2.js
@@ -1,5 +1,5 @@
 /*
-
+Modify the function from the previous exercise so it ignores non-alphabetic characters when determining whether it should uppercase or lowercase each letter. The non-alphabetic characters should still be included in the return value; they just don't count when toggling the desired case.
 */
 // TODO Problem
 /*
@@ -15,14 +15,45 @@ Questions:
 */
 // TODO Examples
 /*
-
+console.log(staggeredCase("I Love Launch School!") === "I lOvE lAuNcH sChOoL!");
+console.log(staggeredCase("ALL CAPS") === "AlL cApS");
+console.log(
+  staggeredCase("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs"
+);
 */
 // TODO Data Structures
 /*
-
-*/
-// TODO Algorithm
-/*
-
+function takes in string
+string converted to array
+set letterCounter = 0
+for each character in array 
+  if letterCounter is even and character is letter, capitalize. add 1 to counter
+  if letterCounter is odd and character is letter, lowercase. add 1 to counter
+  else return character unchanged.
+return result
 */
 // TODO Code
+
+function staggeredCase(string) {
+  let arrayOfChars = string.split("");
+  let letterCounter = 0;
+  return arrayOfChars
+  .map((char) => {
+    if (letterCounter % 2 === 0 && char.match(/[a-z]/gi)) {
+      letterCounter += 1;
+      return char.toUpperCase();
+    }
+    else if (letterCounter % 2 !== 0 && char.match(/[a-z]/gi)) {
+      letterCounter += 1;
+      return char.toLowerCase();
+    }
+    else return char;
+  })
+  .join("");
+}
+
+console.log(staggeredCase("I Love Launch School!") === "I lOvE lAuNcH sChOoL!");
+console.log(staggeredCase("ALL CAPS") === "AlL cApS");
+console.log(
+  staggeredCase("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs"
+);
