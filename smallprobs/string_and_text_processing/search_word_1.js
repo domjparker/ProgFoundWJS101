320639484f4f929d4c179a051513b43b071ba8ec
Rees Dart <thedominicparker@gmail.com>
2020-12-22 07:38:19 -0800

    adding solutions for search_word files.

--- smallprobs/string_and_text_processing/search_word_1.js
+++ smallprobs/string_and_text_processing/search_word_1.js
@@ -1,28 +1,54 @@
 /*
+Write a function that takes a word and a string of text as parameters, and returns an integer representing the number of times the word appears in the text.
 
+You may assume that the word and text inputs will always be provided, and that all word breaks are spaces. Thus, some words will include punctuation such as periods and commas.
 */
 // TODO Problem
 /*
 Explicit Requirements:
-- INPUT =
-- OUTPUT =
-- RULES =
+- INPUT = word string, and text string
+- OUTPUT = number representing how many times word is located within the text
+- RULES = punctuation-sensitive
 
 Implicit Requirements:
-
+-appears to be insensitive to case
 Questions:
-
+-what about words within words?
 */
 // TODO Examples
 /*
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Sed quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?';
 
+searchWord('sed', text);     // 4
+searchWord('est', text);     // 0
+searchWord('est,', text);    // 2
 */
 // TODO Data Structures
 /*
-
+string > array
 */
 // TODO Algorithm
 /*
-
+function takes in stringWord and stringText
+set counter to 0
+convert stringText to array of words
+for each word, does it equal stringWord
+  if so, counter += 1
+return counter
 */
 // TODO Code
+
+const searchWord = (stringWord, stringText) => {
+  let counter = 0;
+  let stringArray = stringText.split(" ");
+  stringArray.forEach(word => {
+    if (word.toLowerCase() === stringWord.toLowerCase()) counter += 1;
+  })
+  return counter;
+};
+
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Sed quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?';
+
+searchWord('sed', text);     // 4
+searchWord('est', text);     // 0
+searchWord('est,', text);    // 2
--- smallprobs/string_and_text_processing/search_word_2.js
+++ smallprobs/string_and_text_processing/search_word_2.js
@@ -1,11 +1,13 @@
 /*
+The function from the previous exercise returns the number of occurrences of a word in some text. Although this is useful, there are also situations in which we just want to find the word in the context of the text.
 
+For this exercise, write a function that takes a word and a string of text as parameters, and returns the text with every instance of the word highlighted. To highlight a word, enclose the word with two asterisks ('**') on each side and change every letter of the word to uppercase (e.g., '**HIGHLIGHTEDWORD**').
 */
 // TODO Problem
 /*
 Explicit Requirements:
-- INPUT =
-- OUTPUT =
+- INPUT = stringWord and stringText
+- OUTPUT = stringText, but with each instance of stringWord highlighted
 - RULES =
 
 Implicit Requirements:
@@ -15,14 +17,38 @@ Questions:
 */
 // TODO Examples
 /*
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Sed quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?';
 
+searchWord('sed', text);
+// returns
+// "**SED** ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, **SED** quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, **SED** quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? **SED** quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?"
 */
 // TODO Data Structures
 /*
-
+string > array 
 */
 // TODO Algorithm
 /*
-
+function takes in stringWord and stringText
+convert stringText to array of words
+map words, 
+  if word(touppercase) equals stringWord(touppercase) return **stringword(touppercase)**
+  else return word
+return mappedWords joined into string
 */
 // TODO Code
+
+const searchWord = (stringWord, stringText) => {
+  let stringArray = stringText.split(" ");
+  return stringArray.map(word => {
+    if (word.toUpperCase() === stringWord.toUpperCase()) {
+      return `**${word.toUpperCase()}**`;
+    } else return word;
+  }).join(" ")
+};
+
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Sed quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?';
+
+searchWord('sed', text);
+// returns
+// "**SED** ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, **SED** quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, **SED** quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? **SED** quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?"
