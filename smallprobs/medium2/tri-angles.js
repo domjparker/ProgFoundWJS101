799d5bac50aef53137b1687230ea97f07e4e7966
Rees Dart <thedominicparker@gmail.com>
2021-01-26 08:04:58 -0800

    adding solutions for tri-angles.js and unlucky_days.js

--- smallprobs/medium2/tri-angles.js
+++ smallprobs/medium2/tri-angles.js
@@ -1,12 +1,25 @@
 /*
+A triangle is classified as follows:
 
+Right: One angle is a right angle (exactly 90 degrees).
+Acute: All three angles are less than 90 degrees.
+Obtuse: One angle is greater than 90 degrees.
+To be a valid triangle, the sum of the angles must be exactly 180 degrees, and every angle must be greater than 0. If either of these conditions is not satisfied, the triangle is invalid.
+
+Write a function that takes the three angles of a triangle as arguments, and returns one of the following four strings representing the triangle's classification: 'right', 'acute', 'obtuse', or 'invalid'.
+
+You may assume that all angles have integer values, so you do not have to worry about floating point errors. You may also assume that the arguments are in degrees.
 */
 // TODO Problem
 /*
 Explicit Requirements:
-- INPUT = 
-- OUTPUT = 
-- RULES =
+- INPUT = 3 integers
+- OUTPUT = string representing if triangle is valid, and if so, what type of triangle
+- RULES = sum of angles must equal 180.
+  each angle must be greater than 0
+  a right angle can only have ONE 90 degree angle
+  an acute angle must have ALL angles below 90 degrees
+  an obtuse angle has ONE angle above 90 degrees
 
 Implicit Requirements:
 
@@ -15,14 +28,79 @@ Questions:
 */
 // TODO Examples
 /*
-
+triangle(60, 70, 50);       // "acute"
+triangle(30, 90, 60);       // "right"
+triangle(120, 50, 10);      // "obtuse"
+triangle(0, 90, 90);        // "invalid"
+triangle(50, 50, 50);       // "invalid"
 */
 // TODO Data Structures
 /*
- 
+numbers > array
 */
 // TODO Algorithm
 /*
+function takes in 3 integers
+  put integers into array
+  if sum of elements < 180 or any element < 1 return "invalid"
+  else if any element === 90, return "right"
+  else if any element > 90, return "obtuse"
+  else return "acute"
 
 */
 // TODO Code
+
+function triangle(num1, num2, num3) {
+  let arr = [num1, num2, num3];
+  if ((arr.reduce((acc, curr) => acc + curr) !== 180) || 
+  (arr.some(element => element < 1))) {
+    return "invalid"
+  } else if (arr.includes(90)) return "right";
+  else if (arr.some(element => element > 90)) return "obtuse";
+  else return "acute";
+}
+
+triangle(60, 70, 50);       // "acute"
+triangle(30, 90, 60);       // "right"
+triangle(120, 50, 10);      // "obtuse"
+triangle(0, 90, 90);        // "invalid"
+triangle(50, 50, 50);       // "invalid"
+
+
+// another way...
+
+function triangle(angle1, angle2, angle3) {
+  const angles = [angle1, angle2, angle3];
+  if (isValid(angles)) {
+    return getTriangleType(angles);
+  } else {
+    return "invalid";
+  }
+}
+
+function isValid(angles) {
+  const totalAngle = angles.reduce((total, angle) => total + angle);
+
+  const allNonZero = angles.every(angle => angle > 0);
+
+  return totalAngle === 180 && allNonZero;
+}
+
+function getTriangleType(angles) {
+  if (angles.some(testRightTriangle)) {
+    return "right";
+  } else if (angles.every(testAcuteTriangle)) {
+    return "acute";
+  } else {
+    return "obtuse";
+  }
+}
+
+function testRightTriangle(angle) {
+  return angle === 90;
+}
+
+function testAcuteTriangle(angle) {
+  return angle < 90;
+}
+
--- smallprobs/medium2/unlucky_days.js
+++ smallprobs/medium2/unlucky_days.js
@@ -1,12 +1,17 @@
 /*
-
+Some people believe that Fridays that fall on the 13th day of the month are considered to be unlucky days. Write a function that takes a year as an argument, and returns the number of Friday the 13ths in that year. You may assume that the year is greater than 1752, which is when the United Kingdom adopted the modern Gregorian Calendar. You may also assume that the same calendar will remain in use for the foreseeable future.
 */
 // TODO Problem
 /*
 Explicit Requirements:
-- INPUT = 
-- OUTPUT = 
-- RULES =
+- INPUT = 4-digit year
+- OUTPUT = number representing how many Fridays fall on the 13th of each month for that year.
+- RULES = year input will be greater than 1752, but can be past current year.
+  will need to use input year to pass iteratively over each month in that year, checking if 13th
+  day falls on 6th day of the week.
+  will need to employ a counter to track matches
+  iteration only needs to happen 12 times.
+
 
 Implicit Requirements:
 
@@ -15,14 +20,53 @@ Questions:
 */
 // TODO Examples
 /*
-
+fridayThe13ths(1986);      // 1
+fridayThe13ths(2015);      // 3
+fridayThe13ths(2017);      // 2
 */
 // TODO Data Structures
 /*
- 
+numbers for dates
 */
 // TODO Algorithm
 /*
+function takes in year
+  set counter to 0
+  for iterative loop from 0 to 11
+    create new Date using year, index, and day (13)
+    if date weekday is equal to 5
+      counter increments by 1
+  return counter
 
 */
 // TODO Code
+
+function fridayThe13ths(year) {
+  let counter = 0;
+  for (let i = 0; i < 12; i += 1) {
+    let dateEachMonth = new Date(year, i, 13);
+    if (dateEachMonth.getDay() === 5) counter += 1;
+  }
+  return counter;
+}
+
+fridayThe13ths(1986);      // 1
+fridayThe13ths(2015);      // 3
+fridayThe13ths(2017);      // 2
+fridayThe13ths(2054);      // 3?
+
+
+
+// could also do...
+
+function fridayThe13ths(year) {
+  let thirteenths = [];
+
+  for (let month = 0; month < 12; month += 1) {
+    thirteenths.push(new Date(year, month, 13));
+  }
+
+  return thirteenths.reduce((count, day) => {
+    return day.getDay() === 5 ? (count + 1) : count;
+  }, 0);
+}
